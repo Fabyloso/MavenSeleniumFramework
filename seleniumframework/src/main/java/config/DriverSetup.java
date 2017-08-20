e904d3275d0d7ea763744b46be67c245fb0d5ba9
fabyloso <faby.loso@hotmail.com>
2017-08-20 14:28:29 -0700

    setup interface for drivers

---
+++ seleniumframework/src/main/java/config/DriverSetup.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public interface DriverSetup {
+
+    WebDriver getWebDriver(DesiredCapabilities desiredCapabilities);
+
+    DesiredCapabilities getDesiredCapabilites();
+
+}
